2f33017bdde79a3833081b2b39a701ac50016fb6
Philippe Lemoine <36411538+phl43@users.noreply.github.com>
2020-12-06 18:44:42 +0100

    Add files via upload

---
+++ Lockdowns, science and voodoo magic.R
@@ -0,0 +1,10 @@
+# NOTE: Flaxman et al.'s code is a mess and I didn't try to fix it, except for a few things that unnecessarily slowed
+# things down, so I apologize if parts of this are difficult to read but I didn't have time to clean everything up
+
+source("Original version published in Nature plus alternative counterfactual/Original version published in Nature plus alternative counterfactual.r")
+
+source("Non-pooled model/Non-pooled model.r")
+
+source("Model with simulated data/Model with simulated data.r")
+
+source("Reproduction number analysis/Reproduction number analysis.r")
